88b838bafb5863b086d2408a1fdf6b07d34c3446
ugololeIvan <ugololeivanrichard@yahoo.com>
2018-03-15 18:16:19 -0400

    update

--- js/custom-java-script.js
+++ js/custom-java-script.js
@@ -37,3 +37,63 @@ function page1(){
 function page2(){
    window.location.href  = 'page2.html';
 }
+
+function page3(){
+   window.location.href = 'page3.html';
+}
+
+function page4(){
+   window.location.href = 'page4.html';
+}
+
+function page5(){
+   window.location.href = 'page5.html';
+}
+
+function page6(){
+   window.location.href = 'page6.html';
+}
+
+function page7(){
+   window.location.href = 'page7.html';
+}
+
+function page8(){
+   window.location.href = 'page8.html';
+}
+
+function page9(){
+   window.location.href = 'page9.html';
+}
+
+function page10(){
+   window.location.href = 'page10.html';
+}
+
+function page11(){
+   window.location.href = 'page11.html';
+}
+
+function page12(){
+   window.location.href = 'page12.html';
+}
+
+function page13(){
+   window.location.href = 'page13.html';
+}
+
+function page14(){
+   window.location.href = 'page14.html';
+}
+
+function page15(){
+   window.location.href = 'page15.html';
+}
+
+function page16(){
+   window.location.href = 'page16.html';
+}
+
+
+
+
